f59a8f9693b55f509545543296dc7be0fa9c70da
Ryan Rho <rho@uber.com>
2017-09-03 16:06:04 -0700

    Break one test into several. Trying out jest :)

--- test/amazon-reload-balance.test.js
+++ test/amazon-reload-balance.test.js
@@ -1,15 +1,23 @@
 import {AmazonReloadBalance} from '../dist';
 
-test('Sign-in input validation', () => {
-  const areNoInputsValid = AmazonReloadBalance.validateSignInInputs(null, null);
-  expect(areNoInputsValid).toBe(false);
+describe('Sign-in input validation', () => {
+  test('no input', () => {
+    const isValid = AmazonReloadBalance.validateSignInInputs(null, null);
+    expect(isValid).toBe(false);
+  })
 
-  const isNoEmailValid = AmazonReloadBalance.validateSignInInputs(null, 'password');
-  expect(isNoEmailValid).toBe(false);
+  test('no email', () => {
+    const isValid = AmazonReloadBalance.validateSignInInputs(null, 'password');
+    expect(isValid).toBe(false);
+  });
 
-  const isNoPasswordValid = AmazonReloadBalance.validateSignInInputs('email', null);
-  expect(isNoPasswordValid).toBe(false);
+  test('no password', () => {
+    const isValid = AmazonReloadBalance.validateSignInInputs('email', null);
+    expect(isValid).toBe(false);
+  });
 
-  const areInputsValid = AmazonReloadBalance.validateSignInInputs('email', 'password');
-  expect(areInputsValid).toBe(true);
+  test('both email and password filled out', () => {
+    const isValid = AmazonReloadBalance.validateSignInInputs('email', 'password');
+    expect(isValid).toBe(true);
+  });
 });
